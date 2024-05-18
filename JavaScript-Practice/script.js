9c48f54aa614f9a34d8402c93c2ae27e42d4a244
koki723cona <kokifilm0820@gmail.com>
2024-05-18 00:58:52 -0700

    js variable

---
+++ JavaScript-Practice/script.js
@@ -0,0 +1,14 @@
+/*
+var name = prompt("what is your name?");
+var firstChar = name.slice(0, 1);
+var upperCasefirstChar = firstChar.toUpperCase();
+var restOfName = name.slice(1, name.length);
+restOfName = restOfName.toLowerCase();
+var capitalisedName = upperCasefirstChar + restOfName;
+alert("Hello, " + capitalisedName); 
+*/
+/*
+var dogAge = prompt("how old is your dog?");
+var humanAge = (dogAge - 2) * 4 + 21;
+alert("Your dog is " + humanAge + " years old in human years.");
+*/
