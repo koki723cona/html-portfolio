85a266d30f1e3f3484832fd6f3785f067e7e584c
koki723cona <kokifilm0820@gmail.com>
2024-06-02 07:43:23 -0700

    higher order function challenge

---
+++ Drum Kit Starting Files/index.js
@@ -0,0 +1,29 @@
+var numberOfDrumButtons = document.querySelectorAll(".drum").length;
+
+for (var i = 0; i<numberOfDrumButtons; i++) {
+  document.querySelectorAll(".drum")[i].addEventListener("click", function () {
+    alert("I got clicked!");
+  });
+}
+
+/*
+function add(num1, num2) {
+  return num1 + num2;
+}
+
+function subtract(num1, num2) {
+  return num1 - num2;
+}
+
+function multiply(num1, num2) {
+  return num1 * num2;
+}
+
+function divide(num1, num2) {
+  return num1 / num2;
+}
+
+function calculator(num1, num2, operator) {
+  return operator(num1, num2);
+}
+*/
