c2ca458d9879aaabeaf21d10ddb40e2f438cce20
koki723cona <kokifilm0820@gmail.com>
2024-06-22 15:12:47 -0700

    finish ejsdata project

--- Backend/4/index.js
+++ Backend/4/index.js
@@ -6,9 +6,25 @@ const port = 3000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get("/", (req, res) => {});
+// Set the view engine to ejs
+app.set("view engine", "ejs");
 
-app.post("/submit", (req, res) => {});
+
+// Serve the form
+app.get("/", (req, res) => {
+  res.render("index.ejs")
+});
+
+// Handle form submission
+app.post("/submit", (req, res) => {
+   const firstName = req.body.fName;
+   const lastName = req.body.lName;
+   const totalLength = firstName.length + lastName.length;
+
+   res.render("index.ejs", {
+    totalLength
+   });
+});
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
